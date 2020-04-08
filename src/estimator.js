7ae223bd8397213d26a9cee56d97617e54557781
damiecode <codenlyn@gmail.com>
2020-04-08 10:25:20 +0100

    complete challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,13 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases } = data;
+
+  // impact estimations
+  const impactCurrentlyInfected = reportedCases * 10;
+  const impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** factor);
+
+  // severe impact estimations
+  const severeImpactCurrentlyInfected = reportedCases * 50;
+  const severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected * (2 ** factor);
+};
 
 export default covid19ImpactEstimator;
